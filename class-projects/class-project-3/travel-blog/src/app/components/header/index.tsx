2d2785e82b1f8f53d353336d2c13bbb49b4ccaaf
IvanLovric2111 <lovricivan055@gmail.com>
2023-10-26 19:10:53 +0200

    Update Class-Project-3

--- class-projects/class-project-3/travel-blog/src/app/components/header/index.tsx
+++ class-projects/class-project-3/travel-blog/src/app/components/header/index.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link'
 
 export const Header = () => {
     return (
-        <section className='flex justify-between  bg-slate-600'>
-            <div className='flex items-center pl-16'>
-                <img className="pr-2"alt="logo"></img>
+        <section className='flex justify-between bg-sky-600'>
+            <div className='flex items-center pl-16 text-white'>
+                <img className="pr-2 text-white" alt="logo"></img>
                 Travel Blog
             </div>
             <div className='flex justify-end pt-4 pb-4 pr-16'>
-                <Link className='pr-8' href="/">Home Page</Link>
-                <Link className="pr-8" href="/pages/destinations">Destinations</Link>
-                <Link className="pr-8" href="/pages/travelStoriesExperiences">Travel Stories&Experiences</Link>
-                <Link className='pr-8' href="/pages/community">Community</Link>
-                <Link className='pr-8' href="/pages/about">About Us</Link>
-                <Link className='pr-8' href="/pages/login">Log In</Link>
+                <Link className='pr-8 text-white' href="/">Home Page</Link>
+                <Link className='pr-8 text-white' href="/pages/destinations">Destinations</Link>
+                <Link className='pr-8 text-white' href="/pages/travelStoriesExperiences">Travel Stories&Experiences</Link>
+                <Link className='pr-8 text-white' href="/pages/community">Community</Link>
+                <Link className='pr-8 text-white' href="/pages/about">About Us</Link>
+                <Link className='pr-8 text-white' href="/pages/login">Log In</Link>
             </div>
         </section>
     )
