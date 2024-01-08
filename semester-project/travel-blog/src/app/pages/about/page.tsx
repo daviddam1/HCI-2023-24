80d1b88dae863b5070909cee2de6ee82664ff11e
daviddam1 <ddamja00@fesb.hr>
2024-01-08 21:37:06 +0100

    fixes

--- semester-project/travel-blog/src/app/pages/about/page.tsx
+++ semester-project/travel-blog/src/app/pages/about/page.tsx
@@ -4,26 +4,26 @@ import React from 'react'
 
 const About = () => {
   return (
-      <section className='bg-[#2A3C4B]'>
-        <Header />
-        <div className='p-20'>
-            <p className='text-[#F2E863] font-bold text-3xl'>About Us</p>
-            <div className='flex flex-end bg-[#F2E863] h-1.5 rounded-xl'></div>
-            <br></br>
-            <p className='text-white pt-8 text-xl'>Welcome to <strong className='text-[#F2E863]'>Traveler</strong>, your passport to a world of exploration and discovery! Founded by David Damjanović and Ivan Lovrić, our blog is a celebration of diverse cultures, breathtaking landscapes, and the joy of uncovering hidden gems around the globe.</p>
-            <div>
-                <br></br>
-                <p className='text-[#F2E863] font-semibold text-2xl'>Meet David:</p>
-                <p className='text-white text-xl pt-3 pl-5 pr-20'>David Damjanović, a dedicated computer science student and avid explorer, merges his passion for technology with his love for travel. From coding challenges to conquering new horizons, David brings a unique perspective to our travel blog. His analytical mind navigates the intricacies of both the digital realm and the diverse landscapes he encounters, providing you with a tech-savvy take on the world's wonders.</p>
-            </div>
-            <div>
-                <br></br>
-                <p className='text-[#F2E863] font-semibold text-2xl'>Meet Ivan:</p>
-                <p className='text-white text-xl pt-3 pl-5 pr-20'>Ivan Lovrić, our co-founder, is a passionate advocate for off-the-beaten-path exploration. Armed with a camera and a keen eye for detail, Ivan captures the essence of each destination, from the tranquil landscapes of the Swiss Alps to the vibrant markets of Marrakech. His goal is to inspire others to venture beyond the ordinary and embrace the extraordinary diversity our world has to offer.</p>
-            </div>
+    <section className='bg-[#2A3C4B]'>
+      <Header />
+      <div className='p-20'>
+        <p className='text-[#F2E863] font-bold text-3xl'>About Us</p>
+        <div className='flex flex-end bg-[#F2E863] h-1.5 rounded-xl'></div>
+        <br></br>
+        <p className='text-white pt-8 text-xl'>Welcome to <strong className='text-[#F2E863]'>Traveler</strong>, your passport to a world of exploration and discovery! Founded by David Damjanović and Ivan Lovrić, our blog is a celebration of diverse cultures, breathtaking landscapes, and the joy of uncovering hidden gems around the globe.</p>
+        <div>
+          <br></br>
+          <p className='text-[#F2E863] font-semibold text-2xl'>Meet David:</p>
+          <p className='text-white text-xl pt-3 pl-5 pr-20'>David Damjanović, a dedicated computer science student and avid explorer, merges his passion for technology with his love for travel. From coding challenges to conquering new horizons, David brings a unique perspective to our travel blog. His analytical mind navigates the intricacies of both the digital realm and the diverse landscapes he encounters, providing you with a tech-savvy take on the world&apos;s wonders.</p>
         </div>
-        <Footer />
-      </section>
+        <div>
+          <br></br>
+          <p className='text-[#F2E863] font-semibold text-2xl'>Meet Ivan:</p>
+          <p className='text-white text-xl pt-3 pl-5 pr-20'>Ivan Lovrić, our co-founder, is a passionate advocate for off-the-beaten-path exploration. Armed with a camera and a keen eye for detail, Ivan captures the essence of each destination, from the tranquil landscapes of the Swiss Alps to the vibrant markets of Marrakech. His goal is to inspire others to venture beyond the ordinary and embrace the extraordinary diversity our world has to offer.</p>
+        </div>
+      </div>
+      <Footer />
+    </section>
   )
 }
 
--- semester-project/travel-blog/src/app/pages/community/page.tsx
+++ semester-project/travel-blog/src/app/pages/community/page.tsx
@@ -17,23 +17,23 @@ const Page = () => {
     <div className='bg-[#2A3C4B]'>
       <Header />
       <div className='text-white flex items-center justify-center'>
-          <p className='text-3xl p-20'>Welcome to <strong className='text-[#F2E863]'>Traveler</strong> Community!</p>
-          <form>
-              <input
-                className="p-2 rounded-xl text-black outline-none"
-                placeholder="Search fellow travelers..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              ></input>
-          </form>
+        <p className='text-3xl p-20'>Welcome to <strong className='text-[#F2E863]'>Traveler</strong> Community!</p>
+        <form>
+          <input
+            className="p-2 rounded-xl text-black outline-none"
+            placeholder="Search fellow travelers..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+        </form>
       </div>
       <div className='grid grid-cols-3 mx-56 mb-24'>
-      {filteredUsers.map((user) => (
-            <div className='flex items-center p-5 text-white'>
-                <Image className='rounded-2xl border-2 border-[#F2E863]' src={user.img} alt='profile image' width='120' height='120'/>
-                <p className='text-xl pl-2'>{user.name}</p>
-            </div>
-      ))}
+        {filteredUsers.map((user) => (
+          <div key={user.id} className='flex items-center p-5 text-white'>
+            <Image className='rounded-2xl border-2 border-[#F2E863]' src={user.img} alt='profile image' width='120' height='120' />
+            <p className='text-xl pl-2'>{user.name}</p>
+          </div>
+        ))}
       </div>
       <Footer />
     </div>
