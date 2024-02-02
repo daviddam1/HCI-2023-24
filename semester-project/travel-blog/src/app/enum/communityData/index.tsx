4bc57b479b98b0180201a10a92afd0c4926de8f0
IvanLovric2111 <lovricivan055@gmail.com>
2024-02-02 19:55:01 +0100

    Fixed

--- semester-project/travel-blog/src/app/enum/communityData/index.tsx
+++ semester-project/travel-blog/src/app/enum/communityData/index.tsx
@@ -2,55 +2,55 @@ const community = [
     {
         id: 1,
         name: 'Alex Foster',
-        img: '/assets/images/alex.png'
+        img: '/assets/images/community/alex.png'
     },
 
     {
         id: 2,
         name: 'Richard Thompson',
-        img: '/assets/images/richard.png'
+        img: '/assets/images/community/richard.png'
     },
 
     {
         id: 3,
         name: 'Sarah Mitchell',
-        img: '/assets/images/sarah.png'
+        img: '/assets/images/community/sarah.png'
     },
 
     {
         id: 4,
         name: 'Emma Turner',
-        img: '/assets/images/emma.png'
+        img: '/assets/images/community/emma.png'
     },
 
     {
         id: 5,
         name: 'Lucas Rodriguez',
-        img: '/assets/images/lucas.png'
+        img: '/assets/images/community/lucas.png'
     },
 
     {
         id: 6,
         name: 'Maya Patel',
-        img: '/assets/images/maya.png'
+        img: '/assets/images/community/maya.png'
     },
 
     {
         id: 7,
         name: 'Ethan Walsh',
-        img: '/assets/images/ethan.png'
+        img: '/assets/images/community/ethan.png'
     },
 
     {
         id: 8,
         name: 'Sofia Kim',
-        img: '/assets/images/sofia.png'
+        img: '/assets/images/community/sofia.png'
     },
 
     {
         id: 9,
         name: 'Oliver Chen',
-        img: '/assets/images/oliver.png'
+        img: '/assets/images/community/oliver.png'
     },
 ];
 
